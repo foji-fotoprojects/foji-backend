5c82a30d7bea9042d3c534e1455c9d7c4c7f7290
Golovastov <kotjane@ya.ru>
2019-11-21 22:04:12 +0300

    Страница редактирования профиля - добавлена валидация телефона на фронте и бэке

--- frontend/views/user/profile.php
+++ frontend/views/user/profile.php
@@ -8,7 +8,6 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
-use yii\bootstrap\Modal;
 
 $this->title = 'Редактирование профиля';
 ?>
@@ -20,7 +19,7 @@ $this->title = 'Редактирование профиля';
         </div>
 
         <h2>Редактирование профиля</h2>
-<!--        --><?php //Pjax::begin(['enablePushState' => false]); ?>
+
             <div id="profile-avatar">
                 <?php if ($profile['avatar_url']) {
                     $avatar = $profile['avatar_url'];
@@ -50,7 +49,11 @@ $this->title = 'Редактирование профиля';
                 <?= $form->field($profile, 'firstName')->label('Имя')->textInput(); ?>
                 <?= $form->field($profile, 'lastName')->label('Фамилия')->textInput(); ?>
                 <?= $form->field($profile, 'sex')->label('Пол')->radioList(['Женский', 'Мужской']); ?>
-                <?= $form->field($profile, 'phone')->label('Телефон')->input('tel'); ?>
+                <?= $form->field($profile, 'phone')->label('Телефон')->input('tel', [
+                        'id' => 'profile_phone',
+                        'pattern' => '^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$',
+                        'placeholder' => '+7(___)___-__-__'
+                    ]); ?>
                 <?= $form->field($profile, 'birthday')->label('Дата рождения')->input('date'); ?>
 
             <label>E-mail</label>
@@ -86,4 +89,39 @@ $this->title = 'Редактирование профиля';
 
     </div>
 </main>
+<!--<script type="text/javascript">-->
+<!--    function setCursorPosition(pos, e) {-->
+<!--        e.focus();-->
+<!--        if (e.setSelectionRange) e.setSelectionRange(pos, pos);-->
+<!--        else if (e.createTextRange) {-->
+<!--            var range = e.createTextRange();-->
+<!--            range.collapse(true);-->
+<!--            range.moveEnd("character", pos);-->
+<!--            range.moveStart("character", pos);-->
+<!--            range.select()-->
+<!--        }-->
+<!--    }-->
+<!---->
+<!--    function mask(e) {-->
+<!--        //console.log('mask',e);-->
+<!--        var matrix = this.placeholder,// .defaultValue-->
+<!--            i = 0,-->
+<!--            def = matrix.replace(/\D/g, ""),-->
+<!--            val = this.value.replace(/\D/g, "");-->
+<!--        def.length >= val.length && (val = def);-->
+<!--        matrix = matrix.replace(/[_\d]/g, function(a) {-->
+<!--            return val.charAt(i++) || "_"-->
+<!--        });-->
+<!--        this.value = matrix;-->
+<!--        i = matrix.lastIndexOf(val.substr(-1));-->
+<!--        i < matrix.length && matrix != this.placeholder ? i++ : i = matrix.indexOf("_");-->
+<!--        setCursorPosition(i, this)-->
+<!--    }-->
+<!--    window.addEventListener("DOMContentLoaded", function() {-->
+<!--        var input = document.querySelector("#profile_phone");-->
+<!--        input.addEventListener("input", mask, false);-->
+<!---->
+<!--        setCursorPosition(3, input);-->
+<!--    });-->
+<!--</script>-->
 
